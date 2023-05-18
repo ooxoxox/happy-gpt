6f02061c640c454459dc06a5e445bd0b3b63be3a
ooxoxox <132582429+ooxoxox@users.noreply.github.com>
2023-05-18 21:43:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def callback():
      #message = TextSendMessage(text=event.message.text)
      message = TextSendMessage(text=random.choice(lunch_options))
      msg = event.message.text
-     if msg == "午餐吃甚麼"
+     if msg == "午餐吃什麼"
          lunch_options = ['便當', '麵類', '飯類', '燉飯', '三明治']
          #message = TextSendMessage(text=event.message.text)
          message = TextSendMessage(text=random.choice(lunch_options))

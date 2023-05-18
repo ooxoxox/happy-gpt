1c0a479ec6ae12ac4aa539d3705d12721203dc2b
ooxoxox <132582429+ooxoxox@users.noreply.github.com>
2023-05-18 21:56:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,10 @@ from linebot.models import *
 import os
 import random
 import openai
-
 app = Flask(__name__)
-
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 openai.api_key = os.environ['OPENAI_SECRET']
-
-
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
@@ -23,7 +19,6 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
@@ -38,15 +33,16 @@ def handle_message(event):
                 )
             # 接收到回覆訊息後，移除換行符號
             reply_msg = TextSendMessage(text=response["choices"][0]["text"].replace('\n',''))
-
     
-    if msg == "午餐吃甚麼":
-        lunch_options = ['便當', '麵類', '飯類', '燉飯', '三明治']
-        #message = TextSendMessage(text=event.message.text)
-        message = TextSendMessage(text=random.choice(lunch_options))
-    line_bot_api.reply_message(event.reply_token, message)
+     if msg == "午餐吃甚麼":
+         lunch_options = ['便當', '麵類', '飯類', '燉飯', '三明治']
+         #message = TextSendMessage(text=event.message.text)
+         message = TextSendMessage(text=random.choice(lunch_options))
+     line_bot_api.reply_message(event.reply_token, message)
+         reply_msg = TextSendMessage(text=random.choice(lunch_options))
+     line_bot_api.reply_message(event.reply_token, reply_msg)
 
-import os
-if __name__ == "__main__":
+ import os
+ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)

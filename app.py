c46a971830dcee5cffbca505763f4e37eae14a4d
ooxoxox <132582429+ooxoxox@users.noreply.github.com>
2023-05-25 21:55:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,56 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    card = {
+              "type": "bubble",
+              "hero": {
+                "type": "image",
+                "url": "https://lions-clubs.dev2.rib.tw/static/documents/images/Lions_Clubs_International.png",
+                "size": "full",
+                "aspectRatio": "1:1",
+                "aspectMode": "cover",
+                "action": {
+                  "type": "uri",
+                  "uri": "http://linecorp.com/"
+                }
+              },
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "113年度獲獎獅友名單",
+                    "weight": "bold",
+                    "size": "xl"
+                  }
+                ]
+              },
+              "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "button",
+                    "style": "link",
+                    "height": "sm",
+                    "action": {
+                      "type": "uri",
+                      "label": "公文連結",
+                      "uri": "https://lions-clubs.dev2.rib.tw"
+                    }
+                  }
+                ],
+                "flex": 0
+              }
+            }
     msg = event.message.text
+    if msg  == "發卡片":
+        line_bot_api.reply_message(
+            event.reply_token,
+            FlexSendMessage(alt_text="發卡片", contents=card))
+        return
     ai_msg = msg[:6].lower()
     if ai_msg == 'hi ai:':
             # 將第六個字元之後的訊息發送給 OpenAI
@@ -51,8 +100,4 @@ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
     
-if msg  == "發卡片":
-        line_bot_api.reply_message(
-            event.reply_token,
-            FlexSendMessage(alt_text="發卡片", contents=card))
-        return
+
